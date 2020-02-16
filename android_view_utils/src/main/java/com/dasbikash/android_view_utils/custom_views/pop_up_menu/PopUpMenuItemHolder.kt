92112517e492a99f96d3600a0bd2716bf117cb55
Bikash Das <das.bikash.asl@gmail.com>
2020-02-16 11:57:20 +0600

    'menu_item_text' type changed to 'TextView'.

--- android_view_utils/src/main/java/com/dasbikash/android_view_utils/custom_views/pop_up_menu/PopUpMenuItemHolder.kt
+++ android_view_utils/src/main/java/com/dasbikash/android_view_utils/custom_views/pop_up_menu/PopUpMenuItemHolder.kt
@@ -1,13 +1,13 @@
 package com.dasbikash.android_view_utils.custom_views.pop_up_menu
 
 import android.view.View
-import androidx.appcompat.widget.AppCompatTextView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dasbikash.android_view_utils.R
 
 internal class PopUpMenuItemHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
 
-    val mMenuItemText:AppCompatTextView = itemView.findViewById(R.id.menu_item_text)
+    val mMenuItemText:TextView = itemView.findViewById(R.id.menu_item_text)
     private val mSeparator:View = itemView.findViewById(R.id.seperator)
 
     fun bind(popUpMenuItem: PopUpMenuItem?, position: Int, itemCount: Int) {
