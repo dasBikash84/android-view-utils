891b29e46c315c4bf2aa451e05ee05c31a8df796
Bikash Das <das.bikash.asl@gmail.com>
2020-02-18 09:34:04 +0600

    'SnackBarUtils' modified.

--- android_view_utils/src/main/java/com/dasbikash/android_view_utils/utils/SnackBarUtils.kt
+++ android_view_utils/src/main/java/com/dasbikash/android_view_utils/utils/SnackBarUtils.kt
@@ -3,50 +3,127 @@ package com.dasbikash.android_view_utils.utils
 import android.graphics.Color
 import android.os.Handler
 import android.os.Looper
-import android.view.View
 import android.widget.TextView
+import androidx.annotation.ColorInt
+import androidx.annotation.StringRes
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.dasbikash.android_view_utils.R
+import com.dasbikash.android_view_utils.utils.SnackBarUtils.DEFAULT_ACTION_TEXT_CASE_IS_UPPER
+import com.dasbikash.android_view_utils.utils.SnackBarUtils.DEFAULT_ACTION_TEXT_COLOR
 import com.google.android.material.snackbar.Snackbar
 
+/**
+ * Helper class to display Snackbar.
+ *
+ * */
 object SnackBarUtils {
 
-    fun showShortSnack(coordinatorLayout: CoordinatorLayout, message: String,
-                       text:CharSequence?=null, listner:()->Unit= {  }) {
-        val snackbar = Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT)
-        text?.let {
-            setSnackbarAction(snackbar, it, listner)
+    internal val DEFAULT_ACTION_TEXT_COLOR = Color.BLUE
+    internal val DEFAULT_ACTION_TEXT_CASE_IS_UPPER = false
+
+    fun showShortSnack(
+        coordinatorLayout: CoordinatorLayout, message: String,
+        actionText: CharSequence? = null, action: () -> Unit = { },
+        @ColorInt actionTextColor:Int=DEFAULT_ACTION_TEXT_COLOR,
+        actionTextUpperCase:Boolean=DEFAULT_ACTION_TEXT_CASE_IS_UPPER
+    ) = showSnack(coordinatorLayout,message,Snackbar.LENGTH_SHORT,
+                    actionText, action, actionTextColor, actionTextUpperCase)
+
+    fun showShortSnack(
+        coordinatorLayout: CoordinatorLayout, @StringRes messageId: Int,
+        actionText: CharSequence? = null, action: () -> Unit = { },
+        @ColorInt actionTextColor:Int=DEFAULT_ACTION_TEXT_COLOR,
+        actionTextUpperCase:Boolean=DEFAULT_ACTION_TEXT_CASE_IS_UPPER
+    ) {
+        coordinatorLayout.context.applicationContext.apply {
+            showSnack(coordinatorLayout, getString(messageId),Snackbar.LENGTH_SHORT,
+                            actionText, action,actionTextColor, actionTextUpperCase)
         }
-        runOnMainThread (task = {
-            snackbar.show()
-        })
     }
 
-    fun showLongSnack(coordinatorLayout: CoordinatorLayout, message: String,
-                      text:CharSequence?=null, listner:()->Unit= {  }) {
-        val snackbar = Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_LONG)
-        text?.let {
-            setSnackbarAction(snackbar, it, listner)
+    fun showLongSnack(
+        coordinatorLayout: CoordinatorLayout, message: String,
+        actionText: CharSequence? = null, action: () -> Unit = { },
+        @ColorInt actionTextColor:Int=DEFAULT_ACTION_TEXT_COLOR,
+        actionTextUpperCase:Boolean=DEFAULT_ACTION_TEXT_CASE_IS_UPPER
+    ) = showSnack(coordinatorLayout,message,Snackbar.LENGTH_LONG,
+        actionText, action, actionTextColor, actionTextUpperCase)
+
+    fun showLongSnack(
+        coordinatorLayout: CoordinatorLayout, @StringRes messageId: Int,
+        actionText: CharSequence? = null, action: () -> Unit = { },
+        @ColorInt actionTextColor: Int = DEFAULT_ACTION_TEXT_COLOR,
+        actionTextUpperCase: Boolean = DEFAULT_ACTION_TEXT_CASE_IS_UPPER
+    ) {
+        coordinatorLayout.context.applicationContext.apply {
+            showSnack(coordinatorLayout, getString(messageId),Snackbar.LENGTH_LONG,
+                            actionText, action,actionTextColor, actionTextUpperCase)
         }
-        runOnMainThread (task =  {
+    }
+
+    private fun showSnack(
+        coordinatorLayout: CoordinatorLayout, message: String,
+        duration:Int,actionText: CharSequence?, action: () -> Unit,
+        @ColorInt actionTextColor: Int,actionTextUpperCase: Boolean
+    ){
+        val snackbar = Snackbar.make(coordinatorLayout, message, duration)
+        actionText?.let {
+            setSnackbarAction(snackbar, it, action,actionTextColor, actionTextUpperCase)
+        }
+        runOnMainThread(task = {
             snackbar.show()
         })
     }
 
     private fun setSnackbarAction(
         snackbar: Snackbar,
-        it: CharSequence,
-        listner: () -> Unit
+        actionText: CharSequence,
+        action: () -> Unit,
+        @ColorInt actionTextColor: Int,
+        actionTextUpperCase: Boolean
     ) {
         snackbar
-            .setAction(it, View.OnClickListener { listner() })
-            .setActionTextColor(Color.WHITE)
+            .setAction(actionText) { runOnMainThread({ action() }) }
+            .setActionTextColor(actionTextColor)
             .getView()
             .findViewById<TextView>(R.id.snackbar_action)
-            .setAllCaps(false)
+            .setAllCaps(actionTextUpperCase)
     }
 }
 
-internal fun runOnMainThread(task: () -> Any?,delayMs:Long=0L){
-    Handler(Looper.getMainLooper()).postDelayed( { task() },delayMs)
+internal fun runOnMainThread(task: () -> Any?, delayMs: Long = 0L) {
+    Handler(Looper.getMainLooper()).postDelayed({ task() }, delayMs)
 }
+
+fun CoordinatorLayout.showShortSnack(
+    message: String,
+    actionText: CharSequence? = null,
+    action: () -> Unit = { },
+    @ColorInt actionTextColor: Int = DEFAULT_ACTION_TEXT_COLOR,
+    actionTextUpperCase: Boolean = DEFAULT_ACTION_TEXT_CASE_IS_UPPER
+) = SnackBarUtils.showShortSnack(this, message, actionText, action,actionTextColor,actionTextUpperCase)
+
+fun CoordinatorLayout.showShortSnack(
+    @StringRes messageId: Int,
+    actionText: CharSequence? = null,
+    action: () -> Unit = { },
+    @ColorInt actionTextColor: Int = DEFAULT_ACTION_TEXT_COLOR,
+    actionTextUpperCase: Boolean = DEFAULT_ACTION_TEXT_CASE_IS_UPPER
+) = SnackBarUtils.showShortSnack(this, messageId, actionText, action,actionTextColor,actionTextUpperCase)
+
+fun CoordinatorLayout.showLongSnack(
+    message: String,
+    actionText: CharSequence? = null,
+    action: () -> Unit = { },
+    @ColorInt actionTextColor: Int = DEFAULT_ACTION_TEXT_COLOR,
+    actionTextUpperCase: Boolean = DEFAULT_ACTION_TEXT_CASE_IS_UPPER
+) = SnackBarUtils.showLongSnack(this, message, actionText, action,actionTextColor,actionTextUpperCase)
+
+fun CoordinatorLayout.showLongSnack(
+    @StringRes messageId: Int,
+    actionText: CharSequence? = null,
+    action: () -> Unit = { },
+    @ColorInt actionTextColor: Int = DEFAULT_ACTION_TEXT_COLOR,
+    actionTextUpperCase: Boolean = DEFAULT_ACTION_TEXT_CASE_IS_UPPER
+) = SnackBarUtils.showLongSnack(this, messageId, actionText, action,actionTextColor,actionTextUpperCase)
+
