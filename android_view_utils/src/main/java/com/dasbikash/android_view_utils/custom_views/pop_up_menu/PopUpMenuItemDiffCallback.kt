b916a8ff3e5ccc8df82b3fa83e07e1c892e1ec68
Bikash Das <das.bikash.asl@gmail.com>
2020-02-18 17:51:29 +0600

    Custom menu view removed.

--- android_view_utils/src/main/java/com/dasbikash/android_view_utils/custom_views/pop_up_menu/PopUpMenuItemDiffCallback.kt
+++
@@ -1,8 +0,0 @@
-package com.dasbikash.android_view_utils.custom_views.pop_up_menu
-
-import androidx.recyclerview.widget.DiffUtil
-
-internal object PopUpMenuItemDiffCallback:DiffUtil.ItemCallback<PopUpMenuItem>() {
-    override fun areItemsTheSame(oldItem: PopUpMenuItem, newItem: PopUpMenuItem)= oldItem == newItem
-    override fun areContentsTheSame(oldItem: PopUpMenuItem, newItem: PopUpMenuItem)= false
-}
--- android_view_utils/src/main/java/com/dasbikash/android_view_utils/utils/AnimationUtils.kt
+++ android_view_utils/src/main/java/com/dasbikash/android_view_utils/utils/AnimationUtils.kt
@@ -1,6 +1,8 @@
 package com.dasbikash.android_view_utils.utils
 
 import android.animation.ObjectAnimator
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 
 fun View.startAlphaAnimation(duration:Long,start:Float=0.0f,end:Float=1.0f) {
@@ -12,4 +14,8 @@ fun View.startRevAlphaAnimation(duration:Long,start:Float=1.0f,end:Float=0.0f) {
     val animation = ObjectAnimator.ofFloat(this, "alpha", start,end)
     animation.duration = duration
     animation.start()
+}
+
+internal fun runOnMainThread(task: () -> Any?, delayMs: Long = 0L) {
+    Handler(Looper.getMainLooper()).postDelayed({ task() }, delayMs)
 }
